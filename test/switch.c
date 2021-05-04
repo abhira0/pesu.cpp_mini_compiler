7b80ca8c065dc77534cc93144b0c681fbc6f0118
abhira0 <abhishekr0@protonmail.com>
2021-05-05 00:03:02 +0530

    added test folder

---
+++ test/switch.c
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+    int i;
+    i = 10;
+    int j;
+    j = i;
+    switch (0)
+    {
+    case 0:
+        int i;
+        int j;
+        j = i * i;
+        j = j * j;
+        break;
+    case 1:
+        int k;
+        break;
+    case 2:
+        int z;
+        z = 12 * 20;
+    case 3:
+        int y;
+    default:
+        int y;
+    }
+    int k;
+    k = 100;
+}
