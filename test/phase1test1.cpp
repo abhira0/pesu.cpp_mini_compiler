8a558396f9fbefdc9c22a0797fce89e5d9a80b44
abhira0 <abhishekr0@protonmail.com>
2021-05-05 00:35:05 +0530

    added phase 1 test cases

---
+++ test/phase1test1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// conversion of int and float
+int main()
+{
+    int int_var;
+    float float_var;
+    int_var = 30;
+    float_var = 3.1415E+2;
+}
---
+++ test/phase1test3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+// generation of [] , [, ]
+int main()
+{
+    int balance[] = {0};
+    double balance[10];
+}
