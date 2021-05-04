d7ca7f62b112a2da0dd794bbb809639883ea6f02
abhira0 <abhishekr0@protonmail.com>
2021-05-05 00:38:48 +0530

    changes in testing code

--- backend/_1_lexer.py
+++ backend/_1_lexer.py
@@ -5,9 +5,9 @@ import ply.lex as lex
 from termcolor import cprint
 
 try:
-    file_code = open("../test/forswitch.cpp", "r").read()
+    file_code = open("../test/for.cpp", "r").read()
 except:
-    file_code = open("./test/forswitch.cpp", "r").read()
+    file_code = open("./test/phase1test1.cpp", "r").read()
 lexer = lex.lex(module=tokrules)
 lexer.input(file_code)
 
@@ -31,8 +31,7 @@ def getIfNumber(value_, type_):
 
 
 # Compute column.
-#     input is the input text string
-#     token is a token instance
+# input is the input text string, token is a token instance
 def find_column(input, token):
     line_start = input.rfind("\n", 0, token.lexpos) + 1
     first_col = (token.lexpos - line_start) + 1
@@ -41,13 +40,16 @@ def find_column(input, token):
 
 
 token_json = {"items": []}
+
 # Tokenize
 for tok in lexer:
     col_range = find_column(file_code, tok)
-    # tok.value = getIfASCII(tok.value, tok.type)
-    # tok.value = getIfNumber(tok.value, tok.type)
+    tok.value = getIfASCII(tok.value, tok.type)
+    tok.value = getIfNumber(tok.value, tok.type)
     json_item = [tok.type, tok.value, tok.lineno, tok.lexpos, col_range]
-    cprint(f"{tok}, Column Range: {col_range}", "green")
     token_json["items"].append(json_item)
+    cprint(f"{tok}, Column Range: {col_range}", "green")
+
+
 with open("symbol_table.json", "w") as f:
     json.dump(token_json, f)
